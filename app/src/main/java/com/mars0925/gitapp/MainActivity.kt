f7b0300d6ebb5f3b313e63dbe2fb4a64041283b1
張宮寶 <mars09250148@gmail.com>
2020-10-07 13:52:51 +0800

    personal data

--- app/src/main/java/com/mars0925/gitapp/MainActivity.kt
+++ app/src/main/java/com/mars0925/gitapp/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name:String = "Mars"
+        val math:Int = 90
+        val eng:Int = 89
     }
 }
